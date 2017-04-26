ceb4160574f5661f593c5fa6326e1b58aa74d131
holi <32414154@qq.com>
2017-04-26 20:51:31 +0800

    test Jackson JavaTimeModule: changes server side dateTimeFormatter pattern to fit client side

--- src/test/java/test/jackson/Jackson2JavaTimeModuleTest.java
+++ src/test/java/test/jackson/Jackson2JavaTimeModuleTest.java
@@ -1,14 +1,16 @@
 package test.jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import org.junit.jupiter.api.Test;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.function.Supplier;
 
@@ -26,22 +28,53 @@ public class Jackson2JavaTimeModuleTest {
 
     @Test
     void changesClientSideDateFormatPatternToFitServerSide() throws Throwable {
-        String json = client(using(SERVER_DATE_TIME_FORMAT_PATTERN)).writeValueAsString(date("2017-04-26 18:29:33"));
+        String json = client(stringify(date("2017-04-26 18:29:33")), using(SERVER_DATE_TIME_FORMAT_PATTERN));
 
-        LocalDateTime it = server().readValue(json);
+        LocalDateTime it = server(parse(json, LocalDateTime.class), using(SERVER_DATE_TIME_FORMAT_PATTERN));
 
         assertThat(it.toString(), equalTo("2017-04-26T18:29:33"));
     }
 
+    @Test
+    void changesServerSideLocalDateTimeFormatPatternToFitClientSide() throws Throwable {
+        String json = client(stringify(date("2017-04-26 18:29:33")), using(CLIENT_DATE_FORMAT_PATTERN));
 
+        LocalDateTime it = server(parse(json, LocalDateTime.class), using(CLIENT_DATE_FORMAT_PATTERN));
 
-    private ObjectWriter client(DateFormat dateFormat) {
-        return objectMapperFactory.get().setDateFormat(dateFormat).writerFor(Date.class);
+        assertThat(it.toString(), equalTo("2017-04-26T18:29:33"));
     }
 
-    private ObjectReader server() {
-        return objectMapperFactory.get().findAndRegisterModules()
-                .readerFor(LocalDateTime.class);
+    private <R> R client(Processor<ObjectMapper, R> processor, SimpleDateFormat dateFormat) throws Throwable {
+        return processor.process(clientObjectMapper(dateFormat));
+    }
+
+    private Processor<ObjectMapper, String> stringify(Date date) {
+        return mapper -> mapper.writer().writeValueAsString(date);
+    }
+
+    private <R> R server(Processor<ObjectMapper, R> processor, SimpleDateFormat dateFormat) throws Throwable {
+        return processor.process(serverObjectMapper(dateFormat));
+    }
+
+    private <R> Processor<ObjectMapper, R> parse(String json, Class<R> type) {
+        return mapper -> mapper.readerFor(type).readValue(json);
+    }
+
+    private ObjectMapper clientObjectMapper(DateFormat dateFormat) {
+        return objectMapperFactory.get().setDateFormat(dateFormat);
+    }
+
+
+    private ObjectMapper serverObjectMapper(SimpleDateFormat dateFormat) {
+        return objectMapperFactory.get().registerModule(javaTimeModule(dateFormat));
+    }
+
+    private SimpleModule javaTimeModule(SimpleDateFormat dateFormat) {
+        return new JavaTimeModule().addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(mapping(dateFormat)));
+    }
+
+    private DateTimeFormatter mapping(SimpleDateFormat dateFormat) {
+        return DateTimeFormatter.ofPattern(dateFormat.toPattern());
     }
 
     private Date date(String date) throws ParseException {
@@ -50,5 +83,9 @@ public class Jackson2JavaTimeModuleTest {
 
     private SimpleDateFormat using(String pattern) {
         return new SimpleDateFormat(pattern);
+    }
+
+    interface Processor<T, R> {
+        R process(T value) throws Throwable;
     }
 }

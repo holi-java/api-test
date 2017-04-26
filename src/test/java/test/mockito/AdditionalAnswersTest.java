d098d4a9554bfa51bd4cc2a651b518d543f43a8a
holi <32414154@qq.com>
2017-04-27 01:35:21 +0800

    enhance the mockito additional answer

---
+++ src/test/java/test/mockito/AdditionalAnswersTest.java
@@ -0,0 +1,49 @@
+package test.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.Answer;
+
+import java.util.function.Function;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static test.mockito.AdditionalAnswersTest.AnswerChain.will;
+
+/**
+ * Created by holi on 4/27/17.
+ */
+class AdditionalAnswersTest {
+
+    @Test
+    void returnMappedValueFromTheFirstArgument() throws Throwable {
+        Function<String, String> function = mock(Function.class);
+
+        when(function.apply(anyString())).then(
+            /**/ will(returnsFirstArg())
+            /**/.as(String.class)
+            /**/.then(String::toUpperCase)
+        );
+
+        assertThat(function.apply("first"), equalTo("FIRST"));
+    }
+
+    interface AnswerChain<T> extends Answer<T> {
+
+        static <T> AnswerChain<T> will(Answer<T> answer) {
+            return answer::answer;
+        }
+
+        default <R> AnswerChain<R> as(Class<R> type) {
+            return (it) -> type.cast(this.answer(it));
+        }
+
+        default <R> AnswerChain<R> then(Function<T, R> mapper) {
+            return (it) -> mapper.apply(this.answer(it));
+        }
+    }
+
+}

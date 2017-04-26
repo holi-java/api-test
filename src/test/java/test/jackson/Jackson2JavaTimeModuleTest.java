adaf83c2e1d5251c845e007e71d92d0c60157237
holi <32414154@qq.com>
2017-04-26 22:54:47 +0800

    make the test more clearly

--- src/test/java/test/jackson/Jackson2JavaTimeModuleTest.java
+++ src/test/java/test/jackson/Jackson2JavaTimeModuleTest.java
@@ -1,7 +1,7 @@
 package test.jackson;
 
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import org.junit.jupiter.api.Test;
@@ -12,8 +12,9 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.function.Supplier;
+import java.util.function.Consumer;
 
+import static java.time.format.DateTimeFormatter.ofPattern;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -24,64 +25,69 @@ public class Jackson2JavaTimeModuleTest {
     private static final String SERVER_DATE_TIME_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss";
     private static final String CLIENT_DATE_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
-    private final Supplier<ObjectMapper> objectMapperFactory = ObjectMapper::new;
-
     @Test
     void changesClientSideDateFormatPatternToFitServerSide() throws Throwable {
-        String json = client(stringify(date("2017-04-26 18:29:33")), using(SERVER_DATE_TIME_FORMAT_PATTERN));
+        String json = client(stringify(date("2017-04-26 18:29:33")), using(dateFormat(SERVER_DATE_TIME_FORMAT_PATTERN)));
 
-        LocalDateTime it = server(parse(json, LocalDateTime.class), using(SERVER_DATE_TIME_FORMAT_PATTERN));
+        LocalDateTime it = server(parse(json, LocalDateTime.class), using(timeModule()));
 
         assertThat(it.toString(), equalTo("2017-04-26T18:29:33"));
     }
 
     @Test
     void changesServerSideLocalDateTimeFormatPatternToFitClientSide() throws Throwable {
-        String json = client(stringify(date("2017-04-26 18:29:33")), using(CLIENT_DATE_FORMAT_PATTERN));
+        String json = client(stringify(date("2017-04-26 18:29:33")), using(dateFormat(CLIENT_DATE_FORMAT_PATTERN)));
 
-        LocalDateTime it = server(parse(json, LocalDateTime.class), using(CLIENT_DATE_FORMAT_PATTERN));
+        LocalDateTime it = server(parse(json, LocalDateTime.class), using(timeModule(ofPattern(CLIENT_DATE_FORMAT_PATTERN))));
 
         assertThat(it.toString(), equalTo("2017-04-26T18:29:33"));
     }
 
-    private <R> R client(Processor<ObjectMapper, R> processor, SimpleDateFormat dateFormat) throws Throwable {
-        return processor.process(clientObjectMapper(dateFormat));
+    private <R> R client(Processor<ObjectMapper, R> processor, Consumer<ObjectMapper> configurer) throws Throwable {
+        return process(processor, configurer);
+    }
+
+    private <R> R server(Processor<ObjectMapper, R> processor, Consumer<ObjectMapper> configurer) throws Throwable {
+        return process(processor, configurer);
+    }
+
+    private <R> R process(Processor<ObjectMapper, R> processor, Consumer<ObjectMapper> configurer) throws Throwable {
+        ObjectMapper mapper = new ObjectMapper();
+        configurer.accept(mapper);
+        return processor.process(mapper);
     }
 
     private Processor<ObjectMapper, String> stringify(Date date) {
         return mapper -> mapper.writer().writeValueAsString(date);
     }
 
-    private <R> R server(Processor<ObjectMapper, R> processor, SimpleDateFormat dateFormat) throws Throwable {
-        return processor.process(serverObjectMapper(dateFormat));
-    }
-
     private <R> Processor<ObjectMapper, R> parse(String json, Class<R> type) {
         return mapper -> mapper.readerFor(type).readValue(json);
     }
 
-    private ObjectMapper clientObjectMapper(DateFormat dateFormat) {
-        return objectMapperFactory.get().setDateFormat(dateFormat);
+    private Consumer<ObjectMapper> using(DateFormat dateFormat) {
+        return it -> it.setDateFormat(dateFormat);
     }
 
-
-    private ObjectMapper serverObjectMapper(SimpleDateFormat dateFormat) {
-        return objectMapperFactory.get().registerModule(javaTimeModule(dateFormat));
+    private Consumer<ObjectMapper> using(Module module) {
+        return it -> it.registerModule(module);
     }
 
-    private SimpleModule javaTimeModule(SimpleDateFormat dateFormat) {
-        return new JavaTimeModule().addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(mapping(dateFormat)));
+    private JavaTimeModule timeModule(DateTimeFormatter formatter) {
+        JavaTimeModule it = timeModule();
+        it.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(formatter));
+        return it;
     }
 
-    private DateTimeFormatter mapping(SimpleDateFormat dateFormat) {
-        return DateTimeFormatter.ofPattern(dateFormat.toPattern());
+    private JavaTimeModule timeModule() {
+        return new JavaTimeModule();
     }
 
     private Date date(String date) throws ParseException {
-        return using(CLIENT_DATE_FORMAT_PATTERN).parse(date);
+        return dateFormat(CLIENT_DATE_FORMAT_PATTERN).parse(date);
     }
 
-    private SimpleDateFormat using(String pattern) {
+    private SimpleDateFormat dateFormat(String pattern) {
         return new SimpleDateFormat(pattern);
     }
 

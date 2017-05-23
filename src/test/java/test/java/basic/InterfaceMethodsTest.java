f80bc628ce42a65e5a1cc38d78617c21fa94c219
holi <32414154@qq.com>
2017-05-23 22:59:12 +0800

    add test of narrowed return type method in interface

--- src/test/java/test/java/basic/InterfaceMethodsTest.java
+++ src/test/java/test/java/basic/InterfaceMethodsTest.java
@@ -13,19 +13,23 @@ import static org.hamcrest.Matchers.*;
  */
 public class InterfaceMethodsTest {
     interface Foo<T> {
+        Object sub();
+
         void any(Object value);
 
         void typed(T value);
     }
 
     interface Bar extends Foo<String> {
+        String sub();
+
         void any(Object value);
 
         void typed(String value);
     }
 
     @Test
-    void redefinedMethodsInInterfaceAreNotBridgeAndSyntheticMethods() throws Throwable {
+    void redefinedMethodsInInterfaceAreNotBothBridgeAndSyntheticMethods() throws Throwable {
         Method it = Bar.class.getDeclaredMethod("any", Object.class);
 
         assertThat(it, not(isSynthentic()));
@@ -39,6 +43,14 @@ public class InterfaceMethodsTest {
 
         assertThat(it, isSynthentic());
         assertThat(it, isBridge());
+    }
+
+    @Test
+    void redefinedNarrowedMethodsInInterfaceAreNotBothBridgeAndSyntheticMethods() throws Throwable {
+        Method it = Bar.class.getDeclaredMethod("sub");
+
+        assertThat(it, not(isSynthentic()));
+        assertThat(it, not(isBridge()));
     }
 
     private Matcher<Method> isBridge() {

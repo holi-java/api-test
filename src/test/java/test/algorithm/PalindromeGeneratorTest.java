150365febd2186af42e34bb42f5ae551010852e4
holi <32414154@qq.com>
2017-05-26 03:26:01 +0800

    transform to `long`

--- src/test/java/test/algorithm/PalindromeGeneratorTest.java
+++ src/test/java/test/algorithm/PalindromeGeneratorTest.java
@@ -16,70 +16,69 @@ public class PalindromeGeneratorTest {
 
     @Test
     void digits1() throws Throwable {
-        assertThat(palindromes(1), equalTo(asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));
+        assertThat(palindromes(1), equalTo(asList(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L)));
     }
 
     @Test
     void digits2() throws Throwable {
-        assertThat(palindromes(2), contains(11, 22, 33, 44, 55, 66, 77, 88, 99));
+        assertThat(palindromes(2), contains(11L, 22L, 33L, 44L, 55L, 66L, 77L, 88L, 99L));
     }
 
     @Test
     void digits3() throws Throwable {
-        List<Integer> result = palindromes(3);
+        List<Long> result = palindromes(3);
 
-        assertThat("head", result.subList(0, 10), equalTo(asList(101, 111, 121, 131, 141, 151, 161, 171, 181, 191)));
-        assertThat("tail", result.subList(80, 90), equalTo(asList(909, 919, 929, 939, 949, 959, 969, 979, 989, 999)));
+        assertThat("head", result.subList(0, 10), equalTo(asList(101L, 111L, 121L, 131L, 141L, 151L, 161L, 171L, 181L, 191L)));
+        assertThat("tail", result.subList(80, 90), equalTo(asList(909L, 919L, 929L, 939L, 949L, 959L, 969L, 979L, 989L, 999L)));
         assertThat(result, hasSize(90));
     }
 
     @Test
     void digits4() throws Throwable {
-        List<Integer> result = palindromes(4);
+        List<Long> result = palindromes(4);
 
-        assertThat("head", result.subList(0, 10), equalTo(asList(1001, 1111, 1221, 1331, 1441, 1551, 1661, 1771, 1881, 1991)));
-        assertThat("tail", result.subList(80, 90), equalTo(asList(9009, 9119, 9229, 9339, 9449, 9559, 9669, 9779, 9889, 9999)));
+        assertThat("head", result.subList(0, 10), equalTo(asList(1001L, 1111L, 1221L, 1331L, 1441L, 1551L, 1661L, 1771L, 1881L, 1991L)));
+        assertThat("tail", result.subList(80, 90), equalTo(asList(9009L, 9119L, 9229L, 9339L, 9449L, 9559L, 9669L, 9779L, 9889L, 9999L)));
         assertThat(result, hasSize(90));
     }
 
     @Test
     void digits5() throws Throwable {
-        List<Integer> result = palindromes(5);
+        List<Long> result = palindromes(5);
 
-        assertThat("head", result.subList(0, 10), equalTo(asList(10001, 10101, 10201, 10301, 10401, 10501, 10601, 10701, 10801, 10901)));
-        assertThat("tail", result.subList(890, 900), equalTo(asList(99099, 99199, 99299, 99399, 99499, 99599, 99699, 99799, 99899, 99999)));
+        assertThat("head", result.subList(0, 10), equalTo(asList(10001L, 10101L, 10201L, 10301L, 10401L, 10501L, 10601L, 10701L, 10801L, 10901L)));
+        assertThat("tail", result.subList(890, 900), equalTo(asList(99099L, 99199L, 99299L, 99399L, 99499L, 99599L, 99699L, 99799L, 99899L, 99999L)));
         assertThat(result, hasSize(900));
     }
 
     @Test
     void digits9() throws Throwable {
-        List<Integer> result = palindromes(9);
+        List<Long> result = palindromes(9);
 
         assertThat(result, hasSize((int) (9 * Math.pow(10, 4))));
-
     }
 
     @Test
-    void max() throws Throwable {
-        List<Integer> result = palindromes(10);
+    void max10() throws Throwable {
+        List<Long> result = palindromes(10);
 
-        assertThat(result.get(result.size() - 1), equalTo(2147447412));
+        assertThat(result.get(result.size() - 1), equalTo(9999999999L));
     }
 
 
-    private static List<Integer> palindromes(int digits) {
+    private static List<Long> palindromes(int digits) {
         return palindromes(digits, 0);
     }
 
-    private static List<Integer> palindromes(int digits, int shifts) {
-        List<Integer> result = new ArrayList<>();
+    private static List<Long> palindromes(int digits, int shifts) {
+        List<Long> result = new ArrayList<>();
         int radix = 10;
-        int high = (int) Math.pow(radix, digits - 1);
+        long high = (long) Math.pow(radix, digits - 1);
         int renaming = digits - 2;
         if (renaming > 0) {
             for (int i = start(digits, shifts); i <= 9; i++) {
-                for (Integer m : palindromes(renaming, shifts + 1)) {
-                    int ret = i * high + m * radix + low(digits, i);
+                for (Long m : palindromes(renaming, shifts + 1)) {
+                    long ret = i * high + m * radix + low(digits, i);
                     if (ret < 0) {//overflow
                         return result;
                     }
